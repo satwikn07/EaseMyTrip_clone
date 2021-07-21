92abbdceb38ccc751948ccd5cb504a0e0c01e723
uudaykuma <uudaykuma@gmail.com>
2021-07-22 00:48:58 +0530

    carousal & mid section

--- src/Components/LandingPage/SearchBox.jsx
+++ src/Components/LandingPage/SearchBox.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from "../../style/searchBox.module.css"
 
 const SearchBox = () => {
     return (
-        <div className={styles.searchBoxMainDiv} >
-            <div className={styles.serach_heading}>
-                <img src="https://i.ibb.co/YQy1MCs/icons8-train-50.png" alt="" />
-                <span>Online Bus Tickets</span></div>
-            <div className={styles.serachBox}>
-                <input placeholder="Source City" type="text" />
+        <div>
+            <div className={styles.searchBoxMainDiv} >
+                <div className={styles.serach_heading}>
+                    <img src="https://i.ibb.co/YQy1MCs/icons8-train-50.png" alt="" />
+                    <span>Online Bus Tickets</span></div>
+                <div className={styles.serachBox}>
+                    <input placeholder="Source City" type="text" />
 
-                <div className={styles.dummyDiv1}>
-                    <div className={styles.line1}></div>
-                    <div className={styles.exchange}><i style={{ color: "grey", width: '15px' }} class="fa fa-exchange" aria-hidden="true"></i></div>
-                    <div className={styles.line1}></div>
+                    <div className={styles.dummyDiv1}>
+                        <div className={styles.line1}></div>
+                        <div className={styles.exchange}><i style={{ color: "grey", width: '15px' }} class="fa fa-exchange" aria-hidden="true"></i></div>
+                        <div className={styles.line1}></div>
+                    </div>
+                    <input placeholder="Destination city" type="text" />
+                    <div className={styles.dummyDiv2}></div>
+                    <input className={styles.date} type="date" />
+                    <button><i class="fa fa-search" aria-hidden="true"></i>&nbsp;SEARCH BUS</button>
                 </div>
-                <input placeholder="Destination city" type="text" />
-                <div className={styles.dummyDiv2}></div>
-                <input className={styles.date} type="date" />
-                <button><i class="fa fa-search" aria-hidden="true"></i>&nbsp;SEARCH BUS</button>
+            </div>
+            <div className={styles.externalPage}>
+                <img onClick={() => alert('clicked')} src="https://www.easemytrip.com/images/bus-img/guidelines-for-bus-travel.png" alt="" />
             </div>
         </div>
     )
--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import SearchBox from '../Components/LandingPage/SearchBox'
+import LandingPage from '../Components/LandingPage/LandingPage'
 import Navbar from '../Components/Navbar'
 
 const Routing = () => {
@@ -9,7 +9,7 @@ const Routing = () => {
             <Navbar />
             <Switch>
                 <Route exact path="/bus">
-                    <SearchBox />
+                    <LandingPage />
                 </Route>
             </Switch>
         </div>
