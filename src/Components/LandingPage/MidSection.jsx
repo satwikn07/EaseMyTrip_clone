eb835e0f5d284543cf02d4c924c44e3f5b1c7776
uudaykuma <uudaykuma@gmail.com>
2021-07-22 23:05:57 +0530

    landing page source and destination search

--- src/Components/LandingPage/MidSection.jsx
+++ src/Components/LandingPage/MidSection.jsx
@@ -1,67 +1,131 @@
-import React from 'react'
-import styles from "../../style/midSection.module.css"
+import React from "react";
+import styles from "../../style/midSection.module.css";
 const MidSection = () => {
-    return (
-        <div>
-            <div className={styles.mid_div}>
-                <h1>Why EaseMyTrip for Bus Booking?</h1>
-                <div className={styles.grid_div}>
-                    <div className={styles.grid1}>
-                        <img className={styles.gridsImages} src="https://www.easemytrip.com/images/bus-img/bus-exclusive-options.png" alt="" />
-                        <h2 className={styles.grid_headings}>Extensive Options</h2>
-                        <p className={styles.grid_div_paragraph} >Book from the selection of buses, including AC, Non-AC, Deluxe and more.</p>
-                    </div>
-                    <div className={styles.grid2}>
-                        <img className={styles.gridsImages} src="https://www.easemytrip.com/images/bus-img/bus-discount2.png" alt="" />
-                        <h2 className={styles.grid_headings}>Discounts or Cashbacks</h2>
-                        <p className={styles.grid_div_paragraph}>We also assure cash backs and reward points to the customers.</p>
-                    </div>
-                    <div className={styles.grid3}>
-                        <img className={styles.gridsImages} src="https://www.easemytrip.com/images/bus-img/bus-payment-option.png" alt="" />
-                        <h2 className={styles.grid_headings}>Payment Options</h2>
-                        <p className={styles.grid_div_paragraph}>Pay through cards, Net banking or mobile wallets for Bus reservation</p>
-                    </div>
-                    <div className={styles.grid4}>
-                        <img className={styles.gridsImages} src="https://www.easemytrip.com/images/bus-img/bus-operator.png" alt="" />
-                        <h2 className={styles.grid_headings}>Book for 2000+ Bus Operators</h2>
-                        <p className={styles.grid_div_paragraph}>We have tied up with best bus operators in India to make travel easy.</p>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.couponInfo}>
-                <h1 className={styles.couponInfo_heading} >Book Cheap Bus Tickets and Get Discount</h1>
-                <div className={styles.couponInfo_description} >
-                    <h4>Book Cheap Bus Tickets and Get Discount</h4>
-                    <p>With simpler and smarter way of online bus booking at EaseMyTrip.com, be ready to travel any destination of India. We list more than 10,000 bus routes across India and provide great facility of online bus reservation. Explore the wide options of buses and choose to travel with Volvo buses, luxury buses, AC/Non-AC buses, sleeper buses or several other buses at reasonable rates. Best discounts and cash back offers are available on bus ticket. Be free from standing in the long queue of bus counters and go for bus booking online with EaseMyTrip.com. Be sure for a memorable & secure journey!</p>
-                </div>
-            </div>
-            <div className={styles.FQA_div}>
-                <h1 className={styles.FAQ_heading}>FAQ</h1>
-                <div className={styles.FAQ_q_a}>
-                    <div>
-                        <h3>Q) Why should I book bus tickets with EaseMyTrip.com?</h3>
-                        <p>A: EaseMyTrip is one of the best platforms to book bus tickets that come up with latest technological updates keeping its users in mind. Wide options for buses are available with great offers and discounts. Book Now</p>
-                    </div>
-                    <div>
-                        <h3>Q) What are the payment methods for booking bus tickets?</h3>
-                        <p>A: With EaseMyTrip bus booking, you have the ease of choosing different methods for payments. Payment options like Debit/Credit Card, Net Banking, Wallets, PhonePe and EMI with various banks are available for the convenience of the customers.</p>
-                    </div>
-                    <div>
-                        <h3>Q) How to avail discount on bus booking with EaseMyTrip?</h3>
-                        <p>A:There are several offers and discounts are available on our website. On this link only, you can find out the ongoing deals on bus tickets .</p>
-                    </div>
-                    <div>
-                        <h3>Q) What to do if I lose my ticket?</h3>
-                        <p>A: The online booking facility allows you having a copy of your bus ticket in your inbox. You can obtain this again from your email I’d.</p>
-                    </div>
-                    <div>
-                        <h3>Q):How can I cancel my bus ticket at EaseMyTrip?</h3>
-                        <p>A:You can cancel your ticket by going to my account segment and then clicking on bus option. The amount of refund will be credited to the original payment method.</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={styles.mid_div}>
+        <h1>Why EaseMyTrip for Bus Booking?</h1>
+        <div className={styles.grid_div}>
+          <div className={styles.grid1}>
+            <img
+              className={styles.gridsImages}
+              src="https://www.easemytrip.com/images/bus-img/bus-exclusive-options.png"
+              alt=""
+            />
+            <h2 className={styles.grid_headings}>Extensive Options</h2>
+            <p className={styles.grid_div_paragraph}>
+              Book from the selection of buses, including AC, Non-AC, Deluxe and
+              more.
+            </p>
+          </div>
+          <div className={styles.grid2}>
+            <img
+              className={styles.gridsImages}
+              src="https://www.easemytrip.com/images/bus-img/bus-discount2.png"
+              alt=""
+            />
+            <h2 className={styles.grid_headings}>Discounts or Cashbacks</h2>
+            <p className={styles.grid_div_paragraph}>
+              We also assure cash backs and reward points to the customers.
+            </p>
+          </div>
+          <div className={styles.grid3}>
+            <img
+              className={styles.gridsImages}
+              src="https://www.easemytrip.com/images/bus-img/bus-payment-option.png"
+              alt=""
+            />
+            <h2 className={styles.grid_headings}>Payment Options</h2>
+            <p className={styles.grid_div_paragraph}>
+              Pay through cards, Net banking or mobile wallets for Bus
+              reservation
+            </p>
+          </div>
+          <div className={styles.grid4}>
+            <img
+              className={styles.gridsImages}
+              src="https://www.easemytrip.com/images/bus-img/bus-operator.png"
+              alt=""
+            />
+            <h2 className={styles.grid_headings}>
+              Book for 2000+ Bus Operators
+            </h2>
+            <p className={styles.grid_div_paragraph}>
+              We have tied up with best bus operators in India to make travel
+              easy.
+            </p>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className={styles.couponInfo}>
+        <h1 className={styles.couponInfo_heading}>
+          Book Cheap Bus Tickets and Get Discount
+        </h1>
+        <div className={styles.couponInfo_description}>
+          <h4>Book Cheap Bus Tickets and Get Discount</h4>
+          <p>
+            With simpler and smarter way of online bus booking at
+            EaseMyTrip.com, be ready to travel any destination of India. We list
+            more than 10,000 bus routes across India and provide great facility
+            of online bus reservation. Explore the wide options of buses and
+            choose to travel with Volvo buses, luxury buses, AC/Non-AC buses,
+            sleeper buses or several other buses at reasonable rates. Best
+            discounts and cash back offers are available on bus ticket. Be free
+            from standing in the long queue of bus counters and go for bus
+            booking online with EaseMyTrip.com. Be sure for a memorable & secure
+            journey!
+          </p>
+        </div>
+      </div>
+      <div className={styles.FQA_div}>
+        <h1 className={styles.FAQ_heading}>FAQ</h1>
+        <div className={styles.FAQ_q_a}>
+          <div>
+            <h3>Q) Why should I book bus tickets with EaseMyTrip.com?</h3>
+            <p>
+              A: EaseMyTrip is one of the best platforms to book bus tickets
+              that come up with latest technological updates keeping its users
+              in mind. Wide options for buses are available with great offers
+              and discounts. Book Now
+            </p>
+          </div>
+          <div>
+            <h3>Q) What are the payment methods for booking bus tickets?</h3>
+            <p>
+              A: With EaseMyTrip bus booking, you have the ease of choosing
+              different methods for payments. Payment options like Debit/Credit
+              Card, Net Banking, Wallets, PhonePe and EMI with various banks are
+              available for the convenience of the customers.
+            </p>
+          </div>
+          <div>
+            <h3>Q) How to avail discount on bus booking with EaseMyTrip?</h3>
+            <p>
+              A:There are several offers and discounts are available on our
+              website. On this link only, you can find out the ongoing deals on
+              bus tickets .
+            </p>
+          </div>
+          <div>
+            <h3>Q) What to do if I lose my ticket?</h3>
+            <p>
+              A: The online booking facility allows you having a copy of your
+              bus ticket in your inbox. You can obtain this again from your
+              email I’d.
+            </p>
+          </div>
+          <div>
+            <h3>Q):How can I cancel my bus ticket at EaseMyTrip?</h3>
+            <p>
+              A:You can cancel your ticket by going to my account segment and
+              then clicking on bus option. The amount of refund will be credited
+              to the original payment method.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MidSection
+export default MidSection;
--- src/Components/LandingPage/SearchBox.jsx
+++ src/Components/LandingPage/SearchBox.jsx
@@ -1,41 +1,143 @@
-import React from 'react'
-import styles from "../../style/searchBox.module.css"
-import { useHistory } from 'react-router';
+import React, { useState } from "react";
+import styles from "../../style/searchBox.module.css";
+import { useHistory } from "react-router";
+
+const defaultSourceCity = [
+  "Hyderabad",
+  "Bangalore",
+  "Chennai",
+  "Delhi",
+  "Goa",
+  "Pune",
+  "Mumbai",
+  "Kolkata",
+];
+const defaultDestinationCity = [
+  "Asansol",
+  "Bangalore",
+  "Bhubaneswar",
+  "Digha",
+  "Durgapur",
+  "Patna",
+  "Puri",
+  "Ranchi",
+  "Siliguri",
+  "Ramgarh",
+  "Angul",
+  "Bokaro",
+];
 
 const SearchBox = () => {
-    const history = useHistory();
+  const [showSourceCity, setShowSourceCity] = useState(false);
+  const [sourceCity, setSourceCity] = useState("");
+  const [showDestCity, setShowDestCity] = useState(false);
+  const [destCity, setDestCity] = useState("");
+  const history = useHistory();
 
+  const nextPage = () => {
+    history.push("/buslist");
+  };
 
-    const nextPage = () =>{
-          history.push("/buslist")
-    }
-    return (
-        <div>
-            <div className={styles.searchBoxMainDiv} >
-                <div className={styles.serach_heading}>
-                    <img src="https://i.ibb.co/YQy1MCs/icons8-train-50.png" alt="" />
-                    <span>Online Bus Tickets</span></div>
-                <div className={styles.serachBox}>
-                    <input placeholder="Source City" type="text" />
+  const handleSourceCity = () => {
+    setShowSourceCity(true);
+    setShowDestCity(false);
+  };
+  const handleDestCity = () => {
+    setShowDestCity(true);
+    setShowSourceCity(false);
+  };
 
-                    <div className={styles.dummyDiv1}>
-                        <div className={styles.line1}></div>
-                        <div className={styles.exchange}><i style={{ color: "grey", width: '15px' }} class="fa fa-exchange" aria-hidden="true"></i></div>
-                        <div className={styles.line1}></div>
-                    </div>
-                    <input placeholder="Destination city" type="text" />
-                    <div className={styles.dummyDiv2}></div>
-                    <input className={styles.date} type="date" />
-                    <button><i class="fa fa-search" aria-hidden="true"></i>&nbsp;SEARCH BUS</button>
-                </div>
-            </div>
-            <div className={styles.externalPage}>
-                <img onClick={nextPage} src="https://www.easemytrip.com/images/bus-img/guidelines-for-bus-travel.png" alt="" />
-            </div>
+  const handleSwapS_to_D = () => {
+    setSourceCity(destCity);
+    setDestCity(sourceCity);
+  };
+  const handleSetCitySource = (e) => {
+    setSourceCity(e.target.textContent);
+    setShowSourceCity(false);
+  };
+  const handleSetDestCity = (e) => {
+    setDestCity(e.target.textContent);
+    setShowDestCity(false);
+  };
+
+  const handleSearchBus = () => {
+      console.log(sourceCity,destCity)
+  }
+  return (
+    <div>
+      <div className={styles.searchBoxMainDiv}>
+        <div className={styles.serach_heading}>
+          <img src="https://i.ibb.co/YQy1MCs/icons8-train-50.png" alt="" />
+          <span>Online Bus Tickets</span>
         </div>
-    )
-}
+        <div className={styles.serachBox}>
+          <input
+            onClick={handleSourceCity}
+            value={sourceCity}
+            onChange={(e) => setSourceCity(e.target.value)}
+            placeholder="Source City"
+            type="text"
+            required="required" 
+          />
+          {showSourceCity ? (
+            <div className={styles.cityNmaeDiv}>
+              {defaultSourceCity.map((item, i) => (
+                <div key={i} onClick={handleSetCitySource}>
+                  <i style={{ margin: "15px" }} class="fas fa-bus-alt"></i>{" "}
+                  {item}
+                </div>
+              ))}
+            </div>
+          ) : null}
 
-export default SearchBox
+          <div className={styles.dummyDiv1}>
+            <div className={styles.line1}></div>
+            <div className={styles.exchange}>
+              <i
+                style={{ color: "grey", width: "15px" }}
+                class="fa fa-exchange"
+                aria-hidden="true"
+                onClick={handleSwapS_to_D}
+              ></i>
+            </div>
+            <div className={styles.line1}></div>
+          </div>
 
+          <input
+            onClick={handleDestCity}
+            value={destCity}
+            onChange={(e) => setDestCity(e.target.value)}
+            placeholder="Destination city"
+            type="text"
+            required="required" 
+          />
+          {showDestCity ? (
+            <div className={styles.DestcityNmaeDiv}>
+              {defaultDestinationCity.map((item, i) => (
+                <div key={i} onClick={handleSetDestCity}>
+                  <i style={{ margin: "15px" }} class="fas fa-bus-alt"></i>{" "}
+                  {item}
+                </div>
+              ))}
+            </div>
+          ) : null}
 
+          <div className={styles.dummyDiv2}></div>
+          <input className={styles.date} type="date" />
+          <button onClick={handleSearchBus}>
+            <i class="fa fa-search" aria-hidden="true"></i>&nbsp; SEARCH BUS
+          </button>
+        </div>
+      </div>
+      <div className={styles.externalPage}>
+        <img
+          onClick={nextPage}
+          src="https://www.easemytrip.com/images/bus-img/guidelines-for-bus-travel.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
