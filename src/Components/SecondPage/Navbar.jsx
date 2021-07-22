c3374aa008c6a139a24d3a162ca44ce90c8ba469
Rajat Sahu <sahurajat78@gmail.com>
2021-07-22 09:38:59 +0530

    filter

--- src/Components/SecondPage/Navbar.jsx
+++ src/Components/SecondPage/Navbar.jsx
@@ -1,12 +1,16 @@
-import { AppBar, Toolbar } from '@material-ui/core'
+import { AppBar, makeStyles, Toolbar } from '@material-ui/core'
+import { useStyles } from '@material-ui/pickers/views/Calendar/Day'
 import React from 'react'
 
+
 export const Navbar = () => {
+
+    const classes = useStyles()
     return (
         <div>
             <AppBar>
                 <Toolbar>
-                    
+
                 </Toolbar>
             </AppBar>
         </div>
